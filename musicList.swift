a0989ddb8178d38d4e98516074e68d21f0119012
Wilson Chen <Wilson@Wilsons-MacBook-Air.local>
2017-01-14 16:33:36 -0700

    media

---
+++ musicList.swift
@@ -0,0 +1,86 @@
+//
+//  musicList.swift
+//  HackerAZ2017
+//
+//  Created by Wilson Chen on 14/01/2017.
+//  Copyright © 2017 Jason Cai. All rights reserved.
+//
+
+import Foundation
+
+struct musicList{
+    
+    var topLibrary = [["index":"0",
+                    "coverImage":"0",
+                    "artist-song":"Mood - Love Is Real",
+                    "location":"Ocean Ave - Cora's Coffee Shoppe",
+                    "upvotes":"165"],
+                    
+                   ["index":"1",
+                    "coverImage":"1",
+                    "artist-song":"Manganas Garden - Day By Day",
+                    "location":"Ocean Ave - Cora's Coffee Shoppe",
+                    "upvotes":"152"],
+                   
+                   ["index":"2",
+                    "coverImage":"2",
+                    "artist-song":"Johnny Stimson - So. Good",
+                    "location":"Ocean Ave - Cora's Coffee Shoppe",
+                    "upvotes":"145"],
+                   
+                   ["index":"3",
+                    "coverImage":"3",
+                    "artist-song":"Gallant - Weight In Gold",
+                    "location":"Ocean Ave - Cora's Coffee Shoppe",
+                    "upvotes":"124"],
+                   
+                   ["index":"4",
+                    "coverImage":"4",
+                    "artist-song":"Katy Perry - California Gurls",
+                    "location":"Ocean Ave - Cora's Coffee Shoppe",
+                    "upvotes":"105"]]
+                   
+
+
+    var recentLibrary = [["index":"5",
+                        "coverImage":"5",
+                        "artist-song":"PINK GUY - HELP",
+                        "location":"Ocean Ave - Cora's Coffee Shoppe",
+                        "upvotes":"6"],
+                      
+                      
+                      ["index":"6",
+                       "coverImage":"6",
+                       "artist-song":"Tom and Laura Misch - Follow",
+                       "location":"Ocean Ave - Cora's Coffee Shoppe",
+                        "upvotes":"1"],
+                      
+                      ["index":"7",
+                       "coverImage":"7",
+                       "artist-song":"Eagles - Hotel California",
+                       "location":"Ocean Ave - Cora's Coffee Shoppe",
+                        "upvotes":"7"],
+                      
+                      ["index":"8",
+                       "coverImage":"8",
+                       "artist-song":"The Chainsmokers - Closer",
+                       "location":"Ocean Ave - Cora's Coffee Shoppe",
+                       "upvotes":"24"],
+                      
+                      ["index":"9",
+                       "coverImage":"9",
+                       "artist-song":"Gallant - Open Up",
+                       "location":"Ocean Ave - Cora's Coffee Shoppe",
+                       "upvotes":"0"]]
+    
+    var dropLibrary = [["index":"10",
+                       "coverImage":"10",
+                       "artist-song":"R. City - Locked Away",
+                       "location":"Ocean Ave - Cora's Coffee Shoppe",
+                       "upvotes":"0"]]
+                     
+                   
+
+    }
+
+
