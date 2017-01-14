daa17e2ceb9fe2240fddc9114d8bd2a95f11cf01
Wilson Chen <Wilson@Wilsons-MacBook-Air.local>
2017-01-14 13:50:55 -0700

    bt

---
+++ musicLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  MusicLibrary.swift
+//  HackerAZ2017
+//
+//  Created by Wilson Chen on 14/01/2017.
+//  Copyright © 2017 Jason Cai. All rights reserved.
+//
+
+//import UIKit
+
+//struct musicLibraryView{
+
+ //   var library = [["title": "Day By Day", "artist": "Manganas Garden", "index":"0", "coverImage":"0"]]
+//}
