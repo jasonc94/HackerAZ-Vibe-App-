f9500742e8362d32395eab09a3322e9ed86458ad
Will <wchen122@asu.edu>
2017-01-15 01:12:52 -0700

    deaf

--- HackerAZ2017/ViewController.swift
+++ HackerAZ2017/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var logo: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        logo.image  = UIImage(named:"logo.jpg")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
