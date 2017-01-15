f43e87be327b578302846479bd57af38f1647953
Wilson Chen <Wilson@Wilsons-MacBook-Air.local>
2017-01-14 18:35:39 -0700

    new stuff

--- musicList.swift
+++ musicList.swift
@@ -11,31 +11,31 @@ import Foundation
 struct musicList{
     
     var topLibrary = [["index":"0",
-                    "coverImage":"0",
+                    "coverImage":"0.jpg",
                     "artist-song":"Mood - Love Is Real",
                     "location":"Ocean Ave - Cora's Coffee Shoppe",
-                    "upvotes"loca:"165"],
+                    "upvotes":"165"],
                     
                    ["index":"1",
-                    "coverImage":"1",
+                    "coverImage":"1.jpg",
                     "artist-song":"Manganas Garden - Day By Day",
                     "location":"Ocean Ave - Cora's Coffee Shoppe",
                     "upvotes":"152"],
                    
                    ["index":"2",
-                    "coverImage":"2",
+                    "coverImage":"2.jpg",
                     "artist-song":"Johnny Stimson - So. Good",
                     "location":"Ocean Ave - Cora's Coffee Shoppe",
                     "upvotes":"145"],
                    
                    ["index":"3",
-                    "coverImage":"3",
+                    "coverImage":"3.jpg",
                     "artist-song":"Gallant - Weight In Gold",
                     "location":"Ocean Ave - Cora's Coffee Shoppe",
                     "upvotes":"124"],
                    
                    ["index":"4",
-                    "coverImage":"4",
+                    "coverImage":"4.jpg",
                     "artist-song":"Katy Perry - California Gurls",
                     "location":"Ocean Ave - Cora's Coffee Shoppe",
                     "upvotes":"105"]]
@@ -43,38 +43,38 @@ struct musicList{
 
 
     var recentLibrary = [["index":"5",
-                        "coverImage":"5",
+                        "coverImage":"5.jpg",
                         "artist-song":"PINK GUY - HELP",
                         "location":"Ocean Ave - Cora's Coffee Shoppe",
                         "upvotes":"6"],
                       
                       
                       ["index":"6",
-                       "coverImage":"6",
+                       "coverImage":"6.jpg",
                        "artist-song":"Tom and Laura Misch - Follow",
                        "location":"Ocean Ave - Cora's Coffee Shoppe",
                         "upvotes":"1"],
                       
                       ["index":"7",
-                       "coverImage":"7",
+                       "coverImage":"7.jpg",
                        "artist-song":"Eagles - Hotel California",
                        "location":"Ocean Ave - Cora's Coffee Shoppe",
                         "upvotes":"7"],
                       
                       ["index":"8",
-                       "coverImage":"8",
+                       "coverImage":"8.jpg",
                        "artist-song":"The Chainsmokers - Closer",
                        "location":"Ocean Ave - Cora's Coffee Shoppe",
                        "upvotes":"24"],
                       
                       ["index":"9",
-                       "coverImage":"9",
+                       "coverImage":"9.jpg",
                        "artist-song":"Gallant - Open Up",
                        "location":"Ocean Ave - Cora's Coffee Shoppe",
                        "upvotes":"0"]]
     
     var dropLibrary = [["index":"10",
-                       "coverImage":"10",
+                       "coverImage":"10.jpg",
                        "artist-song":"R. City - Locked Away",
                        "location":"Ocean Ave - Cora's Coffee Shoppe",
                        "upvotes":"0"]]
