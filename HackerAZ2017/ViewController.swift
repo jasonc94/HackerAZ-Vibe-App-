9682110bb9bec1c1fa61e1c8ca013c74d22db04f
Will <wchen122@asu.edu>
2017-01-15 02:27:57 -0700

    deaf

--- HackerAZ2017/ViewController.swift
+++ HackerAZ2017/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        logo.image  = UIImage(named:"logo.jpg")
+        logo.image  = UIImage(named:"logo.png")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
